bcad0f1d3407dbd22c029b1259a4bcace07a9d8d
Maxime Turin <maxime.turin@gmail.com>
2023-06-06 19:47:51 +0200

    add country_detail & home_page

--- geoLearn/urls.py
+++ geoLearn/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.country_list, name='country_list'),
+    path('', views.home_page, name='home_page'),
+    path('country_list/', views.country_list, name='country_list'),
+    path('country/<int:pk>/', views.country_detail, name='country_detail'),
 ]
--- geoLearn/views.py
+++ geoLearn/views.py
@@ -1,7 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Country
 
 # Create your views here.
 
 
+def home_page(request):
+    return render(request, 'geoLearn/home_page.html', {})
+
+
 def country_list(request):
-    return render(request, 'geoLearn/country_list.html', {})
+    countries = Country.objects.all().order_by("name")
+    return render(request, 'geoLearn/country_list.html', {'countries': countries})
+
+
+def country_detail(request, pk):
+    country = get_object_or_404(Country, pk=pk)
+    return render(request, 'geoLearn/country_detail.html', {'country': country})
